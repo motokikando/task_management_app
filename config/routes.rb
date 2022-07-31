df6bf9453d57f7973dbfde1eb3b803523b959ee1
motokikando <kando813motoki@gmail.com>
2022-07-31 20:32:19 +0900

    change tasks/index root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/',  to: 'tasks#index'
+  get 'tasks/show'
+  get 'tasks/new'
+  get 'tasks/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tasks
 end
